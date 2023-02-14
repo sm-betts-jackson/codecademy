d2861cf0a5eca10afea067a7373acb6f091d4b2d
sm-betts-jackson <sm-betts-jackson@gmail.com>
2023-02-14 11:50:43 -0500

    Almost done, need to debug lives.

--- KittyCafe/KittyCafe.py
+++ KittyCafe/KittyCafe.py
@@ -64,32 +64,42 @@ ENORMOUS = r'''
 (__(__)_____(__)__)/
     ""       ""
 '''
-kitty_images = [teeny_tiny,small,fluffy,chonky,hekkin_big,ENORMOUS]
-
-def intro(kitty_images):
-    input('''
-                           WELCOME TO
+kitty_cafe = '''
       |  /                       ____                  /
       | /  * ----- ----- \   /  /    \    ^    ____  ____  |
       |:   |   |     |    \ /   !        / \   |     |     |
       | \  |   |     |     |    !       /---\  |==   |==   |
       |  \ |   |     |     |    \____/ /     \ |     |___  o
       
-                          Press ENTER...
-      ''') 
+      ''' 
+kitty_images = [teeny_tiny,small,fluffy,chonky,hekkin_big,ENORMOUS]
+life = 3 
+instructions = ['''
+Since you're a new worker here at KittyCafe, 
+it's time to learn how it all works...
+''', '''
+As each cat enters the KittyCafe, you will learn it's
+size, name, hunger level, and favorite food.
+''','''
+It's important that our kitty customers leave the restaurant satisfied,
+so try to remember each cat's favorite food when cooking.
+Cats may have 9 lives, but you only have 3!
+''']
+def intro(kitty_images,kitty_cafe,instructions):
+    print('                           WELCOME TO')
+    print(kitty_cafe)
+    input('Press ENTER...')
+    input(instructions[0])
+    input("Our guests come in all shapes and sizes...\n")
+    input('From teeny tiny...\n'+kitty_images[0])
+    input('to fluffy...\n'+kitty_images[2])
+    input('to absolutely ENORMOUS!!!\n'+kitty_images[-1])
+    input("They are all quite hungry, and can't wait to eat!") 
+    for rule in instructions[1:]:
+        input(rule)
+    print('Time to get started!!!\n')
+    input('Press ENTER...\n')
 
-    print("Our guests come in all shapes and sizes...")
-    time.sleep(3)
-    print('From teeny tiny...\n'+kitty_images[0])
-    time.sleep(2)
-    print('to fluffy...\n'+kitty_images[2])
-    time.sleep(2)
-    print('to absolutely ENORMOUS!!!\n'+kitty_images[-1])
-    time.sleep(3)
-    input("They are all quite hungry, and can't wait to eat!\nPress ENTER...")
-    directions = """
-
-"""
 # In[]
 class Kitty:
     name_list = ["Leo", "Scampers", "Mittens", "Boots", "Charlie", "Buddy", 
@@ -124,61 +134,79 @@ class Kitty:
         elif self.kitty_size == "teeny tiny":
             current_sat_idx = 3
         self.current_sat = self.sat_levels[current_sat_idx]
-        print('A kitty has entered your cafe!')
-        time.sleep(2) 
         print(kitty_images[kitty_size_idx])
-        time.sleep(2)
-        print("This {size} kitty's name is {name}.".format(
+        time.sleep(1)
+        if self.kitty_size == "ENORMOUS":
+            print('An {size} kitty named {name} has entered your cafe!'.format(
                 size=self.kitty_size, name=self.kitty_name))
-        time.sleep(2)
-        print("It's favorite food is {fave}.".format(fave=self.kitty_favorite))
-        time.sleep(2)
-        print("It looks like {name} is \033[1m{sat}\033[0m".format(
+        else:
+            print('A {size} kitty named {name} has entered your cafe!'.format(
+                size=self.kitty_size, name=self.kitty_name))
+        time.sleep(1) 
+        print("{name}'s favorite food is *{fave}*.".format(
+                name=self.kitty_name, fave=self.kitty_favorite))
+        time.sleep(1)
+        print("It looks like {name} is {sat}".format(
                 name=self.kitty_name,sat=self.current_sat))
+        time.sleep(1)
+        print('Better get cooking!')
         time.sleep(2)
-        input('Better get cooking!')
-    def feed_kitty(self,choice,food):
-        print('You served {} to {}'.format(food,self.kitty_name))
-        if choice == self.kitty_favorite:
+    def feed_kitty(self,choice1,choice2,food):
+        time.sleep(1)
+        print('You served {food} to {name}.\n'.format(
+                food=food,name=self.kitty_name))
+        time.sleep(1)
+        if choice1 == self.kitty_favorite or choice2 == self.kitty_favorite:
             current_sat_idx = -1
             current_sat = self.sat_levels[current_sat_idx]
-            print("Excellent! {name} is {sat}/n".format(
+            print("Excellent! {name} is {sat}\n".format(
                     name=self.kitty_name,sat=current_sat))
-        if choice != self.kitty_favorite:
+            time.sleep(1)
+            print('Thanks for feeding {name}!\n'.format(name=self.kitty_name))
+            time.sleep(1)
+            update = input("Will you feed the next cat?\n(Y/N)\n")
+            fave = True
+        if choice1 != self.kitty_favorite and choice2 != self.kitty_favorite:
             if self.size_list.index(self.kitty_size) < self.size_list.index("fluffy"):
-                # still hungry
-            else:
-                # needs a little snack
-            current_sat_idx = 
+                current_sat = self.sat_levels[2]
+                print("It looks like {name} is still {sat}\n".format(
+                        name=self.kitty_name,sat=current_sat))
+            elif self.size_list.index(self.kitty_size) >= self.size_list.index("fluffy"):
+                current_sat = self.sat_levels[-2]
+                print("It looks like {name} is still {sat}\n".format(
+                        name=self.kitty_name, sat=current_sat))
+            time.sleep(1)
+            update = input("Will you feed it more?\n(Y/N)\n")
+            fave = False
+        return update, fave
+                
 def print_options(options):
     for opt in options:
         print(opt)
         if opt == options[-2]:
             print('or')
         if opt == options[-1]:
-            choice= input('???\n')
+            choice = input('???\n')
     return choice
 
 def print_choice(choice,options,meals):
     for i,pick in enumerate(options):
         if choice == pick:
             food = meals[i]
-    return food 
+            return food 
     
 class Cafe:
     ingredients = ['milk','mice','tuna','quail']  
     def cook(self):
-        time.sleep(2)
-        print("Time to add your first ingredient...")
-        time.sleep(2)
-        print("You have these ingredients to start with:")
+        time.sleep(1)
+        print("You have these ingredients to start with:\n")
         time.sleep(1)
         for ingredient in self.ingredients:
             print("*{}*".format(ingredient))
-            time.sleep(1)
-        choice1 = input("Which would you like to add?\n")
+            time.sleep(0.5)
+        choice1 = input("\nWhich would you like to add?")
         time.sleep(1)
-        print("Great! What else would you like to add...")
+        print("\nGreat! What else would you like to add?")
         if choice1 == 'milk':
             options = ['catnip', 'tuna', 'mice', 'nothing']
             meals = ['catnip tea', 'a tuna milkshake', 'a mouse float', 'warm milk']
@@ -186,31 +214,74 @@ class Cafe:
             food = print_choice(choice2,options,meals)
         elif choice1 == 'mice':
             options = ['butter', 'flour','milk', 'nothing']
-            meals = ['buttered mice', 'fried mice', 'a mouse float', 'mouse dinner']
+            meals = ['buttered mice', 'fried mice', 'a mouse float', 'mouse a la carte']
             choice2 = print_options(options)
             food = print_choice(choice2,options,meals)
         elif choice1 == 'tuna':
             options = ['milk','flour','nothing']
-            meals = ['a tuna milkshake', 'tuna patties']
+            meals = ['a tuna milkshake', 'tuna patties', 'tuna a la carte']
             choice2 = print_options(options)
             food = print_choice(choice2,options,meals)
-        if choice1 == 'quail':
+        elif choice1 == 'quail':
             options = ['butter','milk','flour','nothing']
-            meals = ['buttered quail', 'birdie breakfast', 'fried chiken', 'quail a la carte']
+            meals = ['buttered quail', 'birdie breakfast', 'fried chicken', 'quail a la carte']
             choice2 = print_options(options)
             food = print_choice(choice2,options,meals)
         else:
             print('You do not have that ingredient!')
-        return choice1, food
-    def update_satisfaction(self):
-        pass
-# In[]  
-#intro(kitty_images)        
-kat = Kitty()
-kat.generate_kitty()
+            food = "nothing"
+        time.sleep(2)
+        print("\nYou've made {}!\n".format(food))
+        return choice1, choice2, food
+
+# In[] 
+lives = 3
+intro(kitty_images,kitty_cafe,instructions)   
+cat = Kitty()
+cat.generate_kitty()
 cafe = Cafe()
-choice, food = cafe.cook()
-print("You've made {}!".format(food))
+choice1, choice2, food = cafe.cook()
+update, fave = cat.feed_kitty(choice1,choice2,food)
+#True,Y --> new cat
+#True,N --> end game
+#False, Y --> feed cat again
+#False, N --> lose game
+while lives >= 1:
+    print('UPDATE',update,'FAVE',fave)
+    if fave == True:
+        if update == 'Y':
+            cat = Kitty()
+            cat.generate_kitty()
+            cafe = Cafe()
+            choice1, choice2, food = cafe.cook()
+            update, fave = cat.feed_kitty(choice1,choice2,food)
+        elif update == 'N':
+            print("Thank you for serving at\n")
+            print(kitty_cafe)
+    elif fave == False:
+        if update == 'Y':
+            choice,food = cafe.cook()
+            update, fave = cat.feed_kitty(choice,food)
+        elif update == 'N':
+            lives -= 1
+            if lives > 0:
+                print('\nYou made {name} angry!!! =^.v.^='.format(name = cat.kitty_name))
+                print('''
+  ^ v ^ 
+(= *^* =)
+        ''')
+                print("Better be more careful next time!")
+if lives == 0:
+    print(''' 
+                  You're
+           _____  
+           |      _  ___  _
+           |_  * |_) |   |  \  |
+           |   | |\  |=  |   ) |
+           |   | | \ |__ |_ /  *
+           ''')
+    
+    
 
 # In[]
 
