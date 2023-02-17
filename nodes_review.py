bf2ef609f5dd7eb64545552f1b436cf332675acb
sm-betts-jackson <sm-betts-jackson@gmail.com>
2023-02-17 11:35:29 -0500

    added remove_node

--- nodes_review.py
+++ nodes_review.py
@@ -50,8 +50,6 @@ class LinkedList:
   def get_head_node(self):
     return self.head_node
   
-# Add your insert_beginning and stringify_list methods below:
-
   def insert_beginning(self, new_value):
     new_node = Node(new_value)
     new_node.set_next_node(self.head_node)
@@ -66,10 +64,16 @@ class LinkedList:
       current_node = current_node.get_next_node()
     return string_list
   
-
-# Test your code by uncommenting the statements below - did your list print to the terminal?
-ll = LinkedList(5)
-ll.insert_beginning(70)
-ll.insert_beginning(5675)
-ll.insert_beginning(90)
-print(ll.stringify_list())
+  # Define your remove_node method below:
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node
