a19024dddf018bbac639dc4a7250b720ea1d0172
sm-betts-jackson <sm-betts-jackson@gmail.com>
2023-02-24 11:11:45 -0500

    added example for getting nth_last_node

--- nodes_review.py
+++ nodes_review.py
@@ -77,3 +77,27 @@ class LinkedList:
           current_node = None
         else:
           current_node = next_node
+
+# example for finding nth_last_node
+from LinkedList import LinkedList
+
+# Complete this function:
+def nth_last_node(linked_list, n):
+  linked_list_as_list = []
+  current_node = linked_list.head_node
+  while current_node:
+    linked_list_as_list.append(current_node)
+    current_node = current_node.get_next_node()
+  return linked_list_as_list[len(linked_list_as_list) - n]
+
+def generate_test_linked_list():
+  linked_list = LinkedList()
+  for i in range(50, 0, -1):
+    linked_list.insert_beginning(i)
+  return linked_list
+
+# Use this to test your code:
+test_list = generate_test_linked_list()
+print(test_list.stringify_list())
+nth_last = nth_last_node(test_list, 4)
+print(nth_last.value)
